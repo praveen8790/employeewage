c18389828c69b4af25e8f31db59067ffd1510344
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-19 21:43:47 +0530

    done till uc6

--- src/main/java/com/bridgelabz/employeewage/Main.java
+++ src/main/java/com/bridgelabz/employeewage/Main.java
@@ -9,11 +9,13 @@ public class Main {
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HOURS_IN_MONTH = 100;
 
 	public static void main(String args[]) {
 		// VARIABLES
-		int empHrs = 0, empWage = 0, totalEmpWage = 0;
-		for( int day =0;day <NUM_OF_WORKING_DAYS;day++) {
+		int empHrs = 0, empWage = 0, totalEmpWage = 0,totalEmpHrs=0,totalWorkingDays=0;
+		while(totalEmpHrs<= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 			int empCheck = (int)Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 				case IS_FULL_TIME :
@@ -26,11 +28,12 @@ public class Main {
 					empHrs = 0;
 
 			}
+			totalEmpHrs += empHrs;
 			empWage = empHrs * EMP_RATE_PER_HOUR;
 			totalEmpWage += empWage;
 			System.out.println("Emp Wage: " + empWage);
-			
 		}
+		
 		System.out.println("total Emp Wage: " + totalEmpWage);
 	}
 
