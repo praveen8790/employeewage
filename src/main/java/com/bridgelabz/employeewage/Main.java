1aa476afae1eb4fe60750145c648159f8a1f22fc
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-19 20:57:05 +0530

    done till uc1

--- src/main/java/com/bridgelabz/employeewage/Main.java
+++ src/main/java/com/bridgelabz/employeewage/Main.java
@@ -1,10 +1,16 @@
 package com.bridgelabz.employeewage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static final Logger LOG = LogManager.getLogger(Main.class);
+	public static void main(String args[]) {
+		int IS_FULL_TIME = 1;
+		Double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			LOG.debug("Employee is present");
+		else
+			LOG.debug("Employee is absent");
 	}
 
 }
