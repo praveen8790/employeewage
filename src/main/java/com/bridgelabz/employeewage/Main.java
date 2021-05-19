2456d9dc3d25fef71e930ae90e76daf3215ccf41
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-19 21:05:23 +0530

    done till uc2

--- src/main/java/com/bridgelabz/employeewage/Main.java
+++ src/main/java/com/bridgelabz/employeewage/Main.java
@@ -6,11 +6,15 @@ public class Main {
 	private static final Logger LOG = LogManager.getLogger(Main.class);
 	public static void main(String args[]) {
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		//VARIABLES
+		int empHrs = 0;
+		int empWage = 0;
 		Double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
-			LOG.debug("Employee is present");
-		else
-			LOG.debug("Employee is absent");
+			empHrs=8;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 
 }
