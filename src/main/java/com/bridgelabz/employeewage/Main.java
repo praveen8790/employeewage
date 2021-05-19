fadcbee1071d6a8bf39a484ab6fbe32e1c6c3746
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-19 21:29:25 +0530

    done till uc5

--- src/main/java/com/bridgelabz/employeewage/Main.java
+++ src/main/java/com/bridgelabz/employeewage/Main.java
@@ -8,25 +8,30 @@ public class Main {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
 
 	public static void main(String args[]) {
 		// VARIABLES
-		int empHrs = 0;
-		int empWage = 0;
-		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-			case IS_FULL_TIME :
-				empHrs = 8;
-				break;
-			case IS_PART_TIME :
-				empHrs = 4;
-				break;
-			default:
-				empHrs = 0;
+		int empHrs = 0, empWage = 0, totalEmpWage = 0;
+		for( int day =0;day <NUM_OF_WORKING_DAYS;day++) {
+			int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+				case IS_FULL_TIME :
+					empHrs = 8;
+					break;
+				case IS_PART_TIME :
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
 
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Emp Wage: " + empWage);
+			
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " + empWage);
+		System.out.println("total Emp Wage: " + totalEmpWage);
 	}
 
 }
