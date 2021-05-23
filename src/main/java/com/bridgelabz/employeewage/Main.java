da77dc7ea3f8345251205ac59838ef2500cb612a
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-24 03:48:28 +0530

    compute employee wage for multiple companies

--- src/main/java/com/bridgelabz/employeewage/Main.java
+++ src/main/java/com/bridgelabz/employeewage/Main.java
@@ -10,15 +10,37 @@ import org.apache.logging.log4j.Logger;
 public class Main {
 	private static final Logger LOG = LogManager.getLogger(Main.class);
 
-	public static void main(String args[]) {
-		// VARIABLES
-		CalculateWage wage = new CalculateWage();
-		wage.setparam(20,20,100);
-		LOG.debug("total Emp Wage: " + wage.calculate());
-		wage.setparam(200,15,140);
-		LOG.debug("total Emp Wage: " + wage.calculate());
-		
+	public static final int IS_PART_TIME = 1; 
+	public static final int IS_FULL_TIME = 2; 
+	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) { 
+	// Variables 
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0; // Computation 
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) { 
+			totalWorkingDays++; 
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3; 
+			switch (empCheck) { 
+			case IS_PART_TIME: 
+				empHrs = 4; 
+				break; 
+			case IS_FULL_TIME: 
+				empHrs = 8; 
+				break;
+			default: 
+				empHrs = 0; 
+				} 
+			totalEmpHrs += empHrs; 
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
+			
+			} 
+		int totalEmpWage = totalEmpHrs * empRatePerHour; 
+		System.out.println("Total Emp Wage for Company: " +company+" is: "+ totalEmpWage); 
+		return totalEmpWage; 
+	} 
+	public static void main(String[] args) {
+		computeEmpWage("Dmart",20,2,10);
+		computeEmpWage("Reliance",10,4,20);
 		
 	}
+
 
 }
