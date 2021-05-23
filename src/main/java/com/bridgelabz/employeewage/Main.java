c2da7ea4723a9ea903f2b987061fe9e437fd616e
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-24 04:05:45 +0530

    ability to save total wage for each company

--- src/main/java/com/bridgelabz/employeewage/Main.java
+++ src/main/java/com/bridgelabz/employeewage/Main.java
@@ -9,12 +9,23 @@ import org.apache.logging.log4j.Logger;
  */
 public class Main {
 	private static final Logger LOG = LogManager.getLogger(Main.class);
-
 	public static final int IS_PART_TIME = 1; 
 	public static final int IS_FULL_TIME = 2; 
-	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) { 
-	// Variables 
-		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0; // Computation 
+	private final String company;
+	private final int empRatePerHour; 
+	private final int numOfWorkingDays; 
+	private final int maxHoursPerMonth; 
+	private int totalEmpWage; 
+	public Main(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company; 
+		this.empRatePerHour = empRatePerHour; 
+		this.numOfWorkingDays = numOfWorkingDays; 
+		this.maxHoursPerMonth = maxHoursPerMonth; 
+	} 
+	public void computeEmpWage() { 
+		// Variables 
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0; 
+		// Computation 
 		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) { 
 			totalWorkingDays++; 
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3; 
@@ -24,23 +35,25 @@ public class Main {
 				break; 
 			case IS_FULL_TIME: 
 				empHrs = 8; 
-				break;
+				break; 
 			default: 
 				empHrs = 0; 
-				} 
-			totalEmpHrs += empHrs; 
-			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
-			
-			} 
-		int totalEmpWage = totalEmpHrs * empRatePerHour; 
-		System.out.println("Total Emp Wage for Company: " +company+" is: "+ totalEmpWage); 
-		return totalEmpWage; 
+		}
 	} 
-	public static void main(String[] args) {
-		computeEmpWage("Dmart",20,2,10);
-		computeEmpWage("Reliance",10,4,20);
-		
-	}
+		totalEmpHrs += empHrs; 
+		System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs); 
+	} 
+	@Override 
+	public String toString() { return "Total Emp Wage for Company: " +company+ " is: " + totalEmpWage; } 
+	public static void main(String[] args) { 
+		Main dMart = new Main("DMart", 20, 2, 10); 
+		Main reliance = new Main("Reliance", 10, 4, 20); 
+		dMart.computeEmpWage(); 
+		System.out.println(dMart); 
+		reliance.computeEmpWage(); 
+		System.out.println(reliance); 
+	} 
+
 
 
 }
